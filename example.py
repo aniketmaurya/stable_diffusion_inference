a7549491d64f827d6eb0506fc34ca7912ee800b2
Aniket Maurya <aniket@grid.ai>
2022-11-30 14:59:35 -0500

    update

--- example.py
+++ example.py
@@ -3,9 +3,10 @@ from stable_diffusion_inference import SDInference
 config_path = "configs/stable-diffusion/v2-inference-v.yaml"
 checkpoint_path = "https://pl-public-data.s3.amazonaws.com/dream_stable_diffusion/768-v-ema.ckpt"
 
-model = SDInference(
+text2image = SDInference(
     config_path=config_path,
     checkpoint_path=checkpoint_path
     )
 
-model.predict_step("cats in hats")
+image = text2image("cats in hats")
+image.save("cats in hats.png")
