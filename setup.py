52c6eca8a18b2358427e080e1beace9b0a0b1c44
Aniket Maurya <theaniketmaurya@gmail.com>
2022-12-01 00:12:47 +0530

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='stable_diffusion',
+    name='stable_diffusion_inference',
     version='0.0.1',
     description='',
     packages=find_packages(),
