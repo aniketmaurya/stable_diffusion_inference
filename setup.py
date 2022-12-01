c97df1f1ce4528542497bed5bd9c905017fecb6a
Aniket Maurya <theaniketmaurya@gmail.com>
2022-12-01 05:56:47 +0530

    setup requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
-from setuptools import setup, find_packages
+import os
+
+from pkg_resources import parse_requirements
+from setuptools import find_packages, setup
+
+_PATH_ROOT = os.path.dirname(__file__)
+
+def _load_requirements(path_dir: str, file_name: str = "requirements.txt") -> list:
+    reqs = parse_requirements(open(os.path.join(path_dir, file_name)).readlines())
+    return list(map(str, reqs))
+
 
 setup(
     name='stable_diffusion_inference',
@@ -9,4 +19,5 @@ setup(
     install_requires=[
         'torch',
     ],
+    install_requires=_load_requirements(_PATH_ROOT),
 )
