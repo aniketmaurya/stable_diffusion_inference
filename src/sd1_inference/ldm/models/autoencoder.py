b3ee82b3785bf857a9443fb732cbbdef8ba643bd
Aniket Maurya <theaniketmaurya@gmail.com>
2022-12-01 21:41:06 +0530

    fix import

--- src/sd1_inference/ldm/models/autoencoder.py
+++ src/sd1_inference/ldm/models/autoencoder.py
@@ -5,10 +5,10 @@ from contextlib import contextmanager
 
 from taming.modules.vqvae.quantize import VectorQuantizer2 as VectorQuantizer
 
-from ldm.modules.diffusionmodules.model import Encoder, Decoder
-from ldm.modules.distributions.distributions import DiagonalGaussianDistribution
+from sd1_inference.ldm.modules.diffusionmodules.model import Encoder, Decoder
+from sd1_inference.ldm.modules.distributions.distributions import DiagonalGaussianDistribution
 
-from ldm.util import instantiate_from_config
+from sd1_inference.ldm.util import instantiate_from_config
 
 
 class VQModel(pl.LightningModule):
