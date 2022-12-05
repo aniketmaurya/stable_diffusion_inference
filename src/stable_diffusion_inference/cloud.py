6fb5495bf6f8e3274e15ae2c6d34d8d88baa6671
Aniket Maurya <theaniketmaurya@gmail.com>
2022-12-05 14:58:45 +0530

    reformat

--- src/stable_diffusion_inference/cloud.py
+++ src/stable_diffusion_inference/cloud.py
@@ -1,5 +1,7 @@
-import lightning as L
-from lightning.app.components.serve import Image, PythonServer
+from functools import partial
+
+import gradio as gr
+from lightning.app.components.serve import Image, PythonServer, ServeGradio
 from pydantic import BaseModel
 
 
@@ -21,4 +23,22 @@ class SDServe(PythonServer):
         return {"image": self._model(request.prompt)}
 
 
-app = L.LightningApp(SDServe())
+class SDComparison(ServeGradio):
+    inputs = gr.Textbox(label="Prompt", value="Cats in hats")
+    outputs = [gr.Image(label="SD 1"), gr.Image(label="SD 2")]
+    title = "Compare images from Stable Diffusion 1 and 2.0"
+
+    def build_model(self):
+        from stable_diffusion_inference import create_text2image
+
+        sd1 = create_text2image("sd1")
+        sd2 = create_text2image("sd2_base")  # for SD 2.0 with 512 image size
+        return {
+            "sd1": partial(sd1, image_size=512),
+            "sd2": partial(sd2, image_size=512),
+        }
+
+    def predict(self, prompt: str):
+        image1 = self.model["sd1"](prompts=prompt)
+        image2 = self.model["sd2"](prompts=prompt)
+        return [image1, image2]
